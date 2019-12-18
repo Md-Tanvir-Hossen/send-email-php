5bc5ce34a24af26fc4690908c9fd4aaf62d856c4
Codeavoid <42601215+codeavoid@users.noreply.github.com>
2019-12-18 21:02:34 +0530

    Add files via upload

---
+++ sendemail.php
@@ -0,0 +1,41 @@
+<?php
+
+$to = "rajatvarshney2009@gmail.com";
+
+$subject = "Contact Lead";
+
+$message = "<html>
+<head>
+<title>HTML email</title>
+</head>
+<body>
+<p>This email contains HTML Tags!</p>
+<table>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+</tr>
+<tr>
+<td>John</td>
+<td>Doe</td>
+</tr>
+</table>
+</body>
+</html>";
+
+$header = 'From: <contact@codeavoid.com>' . "\r\n";
+$header .= 'Content-type:text/html;charset=UTF-8' . "\r\n";
+
+
+if(mail($to,$subject,$message,$header)){
+
+echo "Email send successfully";
+
+}else{
+
+echo "Email Failed";
+
+}
+
+
+?>
